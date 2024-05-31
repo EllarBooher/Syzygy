d4d75712d291a1252dbbc3c322bd3bc9bb05e097
Lucas Booher <15761263+EllarBooher@users.noreply.github.com>
2024-05-30 18:41:57 -0700

    Removed unused UI method

--- source/vulkan-renderer/source/ui/engineui.cpp
+++ source/vulkan-renderer/source/ui/engineui.cpp
@@ -342,39 +342,6 @@ void imguiStructureControls<CameraParameters>(
 }
 
 template<>
-void imguiStructureDisplay<DrawResultsGraphics>(
-    DrawResultsGraphics const& structure
-)
-{
-    ImGui::BeginGroup();
-    ImGui::Text("Graphics Draw Results");
-    ImGui::Indent(10.0);
-
-    ImGui::BeginTable("drawResultsGraphics", 2);
-
-    ImGui::TableNextColumn();
-    ImGui::Text("Draw Calls");
-    ImGui::TableNextColumn();
-    ImGui::Text("%u", structure.drawCalls);
-
-    ImGui::TableNextColumn();
-    ImGui::Text("Vertices Drawn");
-    ImGui::TableNextColumn();
-    ImGui::Text("%u", structure.verticesDrawn);
-
-    ImGui::TableNextColumn();
-    ImGui::Text("Indices Drawn");
-    ImGui::TableNextColumn();
-    ImGui::Text("%u", structure.indicesDrawn);
-
-    ImGui::EndTable();
-
-    ImGui::Unindent(10.0);
-
-    ImGui::EndGroup();
-}
-
-template<>
 void imguiStructureControls<DebugLines>(
     DebugLines& structure
 )
