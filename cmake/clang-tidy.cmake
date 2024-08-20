c4468371f2cb978d1abd33e347479fdda56c7b53
Lucas Booher <15761263+EllarBooher@users.noreply.github.com>
2024-08-19 17:05:05 -0700

    Updated clang-tidy to work with header files using CMake integration.

--- cmake/clang-tidy.cmake
+++ cmake/clang-tidy.cmake
@@ -1,5 +1,4 @@
 option(CLANG_TIDY_ENABLE "Enable clang-tidy - this impacts compilation time." OFF)
-set(CLANG_TIDY_OPTIONS CACHE STRING "Additional options to pass to clang-tidy, e.g. -fix")
 
 if (NOT CLANG_TIDY_ENABLE)
 	message(STATUS "clang-tidy NOT enabled")
@@ -13,20 +12,52 @@ find_program(
 	DOC 
 		"The path of clang-tidy, ran alongside compilation. Linting rules are found in '.clang-tidy'."
 )
+find_program(
+	CLANG_APPLY_REPLACEMENTS_PATH
+	NAMES 
+		"clang-apply-replacements"
+	DOC 
+		"The path of clang-apply-replacements, used to apply the fixes output by clang-tidy."
+)
 
 if (CLANG_TIDY_PATH)
-	set(
-		CLANG_TIDY_WITH_OPTIONS 
-			"${CLANG_TIDY_PATH}" "${CLANG_TIDY_OPTIONS}"
-	)
-	message(STATUS "clang-tidy enabled - using ${CLANG_TIDY_WITH_OPTIONS}")
+	message(STATUS "clang-tidy enabled - using ${CLANG_TIDY_PATH}")
 
 	set_property(
 		TARGET 
 			syzygy
 		PROPERTY 
 			CXX_CLANG_TIDY 
-				"${CLANG_TIDY_WITH_OPTIONS}"
+				"${CLANG_TIDY_PATH}"
+	)
+
+	set(FIX_DIR_RELATIVE "clang-tidy-fixes-dir")
+	set(FIX_FULL_DIR "${CMAKE_BINARY_DIR}/syzygy/${FIX_DIR_RELATIVE}")
+	message(STATUS "clang-tidy fixes output to ${FIX_FULL_DIR}")
+
+	set_target_properties(
+		syzygy
+		PROPERTIES
+			CXX_CLANG_TIDY
+				"${CLANG_TIDY_PATH}"
+			CXX_CLANG_TIDY_EXPORT_FIXES_DIR
+				"clang-tidy-fixes-dir"
+	)
+	add_custom_target(
+		clang-tidy-apply-fixes 
+		COMMAND
+			"${CLANG_APPLY_REPLACEMENTS_PATH}"
+			"${FIX_FULL_DIR}"
+		WORKING_DIRECTORY
+			"${CMAKE_SOURCE_DIR}"
+		COMMENT
+			"clang-apply-replacements: Applying replacements in ${FIX_FULL_DIR}"
+	)
+	set_target_properties(
+		clang-tidy-apply-fixes 
+		PROPERTIES 
+			EXCLUDE_FROM_ALL 
+				true
 	)
 else()
 	message(WARNING "clang-tidy enabled - unable to find program, or no path is specified")
